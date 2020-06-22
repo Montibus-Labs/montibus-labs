5bf6debbfd0fcbf8c561f76587c48d31d2054afd
Cody Norman <wcodynorman@gmail.com>
2020-06-22 11:37:34 -0600

    add netlify form

--- src/subpages/End.js
+++ src/subpages/End.js
@@ -11,26 +11,30 @@ export default function End() {
         <div className="flexbox">
           <Fade left>
             <h1 className="text-center my-5">Stay in touch</h1>
-            <div className="row my-4">
-              <div className="col-lg-6 mb-4">
-                <input
-                  type="name"
-                  className="inputBox"
-                  placeholder="Your Name"
-                />
+            <form method="post" netlify-honeypot="bot-field" data-netlify="true" name="contact">
+              <input type="hidden" name="bot-field" />
+              <input type="hidden" name="form-name" value="contact" />
+              <div className="row my-4">
+                <div className="col-lg-6 mb-4">
+                  <input
+                    type="name"
+                    className="inputBox"
+                    placeholder="Your Name"
+                  />
+                </div>
+                <div className="col-lg-6 mb-4">
+                  <input
+                    type="email"
+                    className="inputBox"
+                    placeholder="Your Email"
+                  />
+                </div>
               </div>
-              <div className="col-lg-6 mb-4">
-                <input
-                  type="email"
-                  className="inputBox"
-                  placeholder="Your Email"
-                />
-              </div>
-            </div>
-            <textarea rows = "2" className="inputBox mb-4" placeholder="Your Message" />
-            <button className="btn btn-light mb-4 py-3 submitButton">
-              Submit
-            </button>
+              <textarea rows = "2" className="inputBox mb-4" placeholder="Your Message" />
+              <button className="btn btn-light mb-4 py-3 submitButton" type="submit" >
+                Submit
+              </button>
+            </form>
           </Fade>
         </div>
       </div>
