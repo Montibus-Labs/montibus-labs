25deb6c9294bb2bb2fbfc2c6b2defa3d56317da3
Cody Norman <wcodynorman@gmail.com>
2020-06-23 10:25:34 -0600

    center services and remove placeholders

--- src/subpages/Services.js
+++ src/subpages/Services.js
@@ -13,36 +13,36 @@ function Services() {
           <h1 className="text-center my-5">Services We Offer </h1>
           <div className="row">
             <Fade bottom>
-              <div className="col-lg-3 my-4 col-sm-12">
+              <div className="col-lg-3 my-4 col-sm-12 text-center">
                 <FontAwesomeIcon icon={faRocket} size="8x" />
                 <h3 className="mt-5">Branding</h3>
                 <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus
-                  harum eum odit tempore quos dolor nesciunt rerum est totam alias.
+                  First impressions matter.  Let us help you breathe life into
+                  your vision with our industry leading branding services.
                 </p>
               </div>
-              <div className="col-lg-3 my-4 col-sm-12">
+              <div className="col-lg-3 my-4 col-sm-12 text-center">
                 <FontAwesomeIcon icon={faMagic} size="8x" />
                 <h3 className="mt-5">Design</h3>
                 <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus
-                  harum eum odit tempore quos dolor nesciunt rerum est totam alias.
+                  Mobile first designs focused on conversions.  We go much further than
+                  just a logo.  Let us help get your ideas out into the world.
                 </p>
               </div>
-              <div className="col-lg-3 my-4 col-sm-12">
+              <div className="col-lg-3 my-4 col-sm-12 text-center">
                 <FontAwesomeIcon icon={faCode} size="8x" />
                 <h3 className="mt-5">Development</h3>
                 <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus
-                  harum eum odit tempore quos dolor nesciunt rerum est totam alias.
+                  Specializing in creating MVP applications, our team of industry veterans has years of experience
+                  launching new projects from square 0.
                 </p>
               </div>
-              <div className="col-lg-3 my-4 col-sm-12">
+              <div className="col-lg-3 my-4 col-sm-12 text-center">
                 <FontAwesomeIcon icon={faCommentsDollar} size="8x" />
                 <h3 className="mt-5">Marketing</h3>
                 <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus
-                  harum eum odit tempore quos dolor nesciunt rerum est totam alias.
+                  Don't let your new project collect dust after launching.  We offer content, email,
+                  social media, SEO and PPC services to find and convert your audience.
                 </p>
               </div>
             </Fade>
